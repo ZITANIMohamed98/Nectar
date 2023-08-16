4ed453cc456fe7e05d656c92cfdc34d461c61850
ZITANIMohamed98 <zitaniceo@gmail.com>
2023-08-16 21:19:42 +0200

    update media

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
-
+import {}
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='app__navbar'>
+      <div className='app__navbar__container'>
+        <div className='app__navbar__logo'>
+          <p>Nectar</p>
+        </div>
+        <div className='app__navbar__links'>
+          <a href='#'>Home</a>
+          <a href='#'>About</a>
+          <a href='#'>Contact</a>
+        </div>
+        <div className='app__navbar__search'>
+          <input type='text' placeholder='Search...' />
+              <img src='./search.svg' alt='search' />
+          </div>
+      </div>
+    </div>
+
   )
 }
 
---
+++ src/constants/images.js
@@ -0,0 +1,51 @@
+import aboutUs from '../assets/AboutUs-img.png';
+import arrowLeft from '../assets/arrow-left.png';
+import arrowRight from '../assets/arrow-right.png';
+import avocado from '../assets/avocado.png';
+import call from '../assets/Call.svg';
+import email from '../assets/Email.svg';
+import facebook from '../assets/facebook.svg';
+import freeDelivery from '../assets/free-delivery.png';
+import growth from '../assets/growth.png';
+import instagram from '../assets/instagram.svg';
+import jojobaOil from '../assets/jojoba-oil.svg';
+import palmOil from '../assets/palm-oil.png';
+import palmBased from '../assets/plant-based.png';
+import quality from '../assets/quality-assurance.png';
+import reviewsLeft from '../assets/reviews-branch-left.svg';
+import reviewsRight from '../assets/reviews-branch-right.svg';
+import sourceLeft from '../assets/source-branch-left.svg';
+import sourceRight from '../assets/source-branch-right.svg';
+import star from '../assets/star.png';
+import testimonial01 from '../assets/testimonial-Ainee.svg';
+import testimonial02 from '../assets/testimonial-Alina.svg';
+import testimonial03 from '../assets/testimonial-Sophie.svg';
+import twitter from '../assets/twitter.svg';
+import header from '../assets/Header-img.png';
+
+export default {
+    aboutUs,
+    arrowLeft,
+    arrowRight,
+    avocado,
+    call,
+    email,
+    facebook,
+    freeDelivery,
+    growth,
+    instagram,
+    jojobaOil,
+    palmOil,
+    palmBased,
+    quality,
+    reviewsLeft,
+    reviewsRight,
+    sourceLeft,
+    sourceRight,
+    star,
+    testimonial01,
+    testimonial02,
+    testimonial03,
+    twitter,
+    header,
+};
